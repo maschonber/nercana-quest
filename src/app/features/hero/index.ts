335ca02a6e25282e5f0c2da01b6daf7902aa0326
Maschonber <mart.schoenberger@gmail.com>
2025-05-29 09:04:52 +0200

    Auto-generated commit

--- src/app/features/hero/index.ts
+++
@@ -1,5 +0,0 @@
-// Hero feature exports
-export * from './models/hero.model';
-export * from './services/hero-domain.service';
-export * from './services/hero-facade.service';
-export * from './components/hero-details.component';
